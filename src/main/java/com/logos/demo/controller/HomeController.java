5fe17ac362bcc7cb7f8f57bf6df175fe470d0bef
uksta <uksta>
2016-04-19 19:46:54 +0300

    changes in home controller

--- src/main/java/com/logos/demo/controller/HomeController.java
+++ src/main/java/com/logos/demo/controller/HomeController.java
@@ -18,6 +18,7 @@ public class HomeController {
 	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
 	public String home(Model model, HttpServletRequest request) {
 
+		int a = 1;
 		return "home";
 	}
 
